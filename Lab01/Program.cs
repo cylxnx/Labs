4825053c69d94ed43f943bd8344e5e83c81a1a56
clane15 <clane15@PC-BS05333>
2024-09-16 17:30:12 -0400

    Add project files.

---
+++ Lab01/Program.cs
@@ -0,0 +1,23 @@
+﻿//Cody Lane
+//clane15@stumail.northeaststate.edu
+namespace Lab01
+{
+    /// <summary>
+    /// Lab01 Implementation
+    /// With a Bonus Beep!
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Program Entry Point
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Cody Lane");
+            Console.WriteLine("codylane461@gmail.com");
+            Console.WriteLine("All the meats, some black olives and maybe a mushroom or two");
+            Console.Beep();
+        }
+    }
+}

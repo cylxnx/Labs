20d02e31b5fd236ee03aff8fdf2f1677306f573b
clane15@stumail.northeaststate.edu <clane15@stumail.northeaststate.edu>
2024-10-21 22:56:16 -0400

    Completed Lab10

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -19,81 +19,87 @@ namespace Lab10
 
             Console.Write("Enter a month in the year (e.g., 1 for Jan): ");
             tempInput = Console.ReadLine();
-            month = Convert.ToInt16(tempInput);
+            month = Convert.ToInt32(tempInput);
             
             Console.Write("Enter a year: ");
             tempInput = Console.ReadLine();
-            year = Convert.ToInt16(tempInput);
+            year = Convert.ToInt32(tempInput);
+
 
             switch (month)
             {
                 case 1:
                     monthLabel = "January";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 2:
                     monthLabel = "February";
-                    if (year % 400 == 0 || year % 4 && year % 100 != 0)
+                    if (year % 400 == 0 || year % 4 == 0 && year % 100 !=0)
                     {
                         days = 29;
+                        Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     }
                     else
-                    { 
+                    {
                         days = 28;
+                        Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     }
                     break;
                 case 3:
                     monthLabel = "March";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 4:
                     monthLabel = "April";
                     days = 30;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 5:
                     monthLabel = "May";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 6:
                     monthLabel = "June";
                     days = 30;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 7:
                     monthLabel = "July";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 8:
                     monthLabel = "August";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 9:
                     monthLabel = "September";
                     days = 30;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 10:
                     monthLabel = "October";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 11:
                     monthLabel = "November";
                     days = 30;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
                     break;
                 case 12:
                     monthLabel = "December";
                     days = 31;
+                    Console.WriteLine($"{monthLabel} {year} has {days} days.");
+                    break;
+                default:
                     break;
             }
 
-            string message = $"{monthLabel}{year}has{days}days";
-            Console.WriteLine(message);
-
-            
-        
-
-
-
-
-                
 
 
 
@@ -105,9 +111,19 @@ namespace Lab10
 
 
 
-            }
+
+
+
+
+
+
+
+
 
 
         }
+
+
     }
 }
+    
